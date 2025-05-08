515ffac73b4997da83aeb0906928d22a49372872
ShaliniSingh-debug <75964089+ShaliniSingh-debug@users.noreply.github.com>
2025-05-08 09:12:54 -0400

    [3024] [Time Beats: 100.00%] [Memory Beats: 33.10%] - LeetPush

---
+++ Type-of-Triangle.py
@@ -0,0 +1,18 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        x,y,z=nums
+
+        if x+y>z and y+z>x and z+x>y:
+            if x == y == z:
+                return "equilateral"
+                
+            elif x==y or y==z or z == x:
+                return "isosceles"
+            else:
+                return "scalene"
+        else:
+            return "none"
+
+          
+            
+        
