e10b7aa49deb041514e7b1c9928bca3e16e14343
ShaliniSingh-debug <75964089+ShaliniSingh-debug@users.noreply.github.com>
2025-05-07 20:11:51 -0400

    Time: 144 ms (87.31%) | Memory: 19 MB (29.75%) - LeetSync

--- Leet_code_daily/907-koko-eating-bananas/koko-eating-bananas.py
+++ Leet_code_daily/907-koko-eating-bananas/koko-eating-bananas.py
@@ -11,7 +11,7 @@ class Solution:
 
         while l<r:
             k = (l+r)//2
-            # if the p/k value gives the value which is near to hours we get the value but if not we make l as the value we check bigger than that like if 2 per hour banana was not enough and l was 3 then we make l as 3
+            
             if k_works(k): 
                 r= k
             else:
