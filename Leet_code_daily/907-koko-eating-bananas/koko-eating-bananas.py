f73592d2b248aae618843087bcddf1ed2e0fb672
ShaliniSingh-debug <75964089+ShaliniSingh-debug@users.noreply.github.com>
2025-05-07 20:12:17 -0400

    Time: 135 ms (96.16%) | Memory: 18.9 MB (75.59%) - LeetSync

--- Leet_code_daily/907-koko-eating-bananas/koko-eating-bananas.py
+++ Leet_code_daily/907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,13 +1,12 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # check the hours devide by k banana eating from pile per hour 
         def k_works(k):
             hours = 0
             for p in piles:
                 hours +=ceil(p/k)
             return hours<=h
         l =1
-        r = max(piles) #the maximum value in pile is 11 
+        r = max(piles)
 
         while l<r:
             k = (l+r)//2
