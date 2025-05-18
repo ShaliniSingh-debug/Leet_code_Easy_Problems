452238ed74d7c7f5c8707746e609b5bd5f5a35a4
ShaliniSingh-debug <75964089+ShaliniSingh-debug@users.noreply.github.com>
2025-05-18 11:22:26 -0400

    Time: 1 ms (33.80%) | Memory: 17.7 MB (90.73%) - LeetSync

--- Leet_code_daily/83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ Leet_code_daily/83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -9,7 +9,7 @@ class Solution:
             return None
         curr= head
         while curr and curr.next: # check if the curr and curr.next node is present , if yes
-            if curr.val==curr.next.val: # check if the curr and curr.next value and are same if yes remove the node by next = next .next
+            if curr.val==curr.next.val: 
                 curr.next= curr.next.next
             else:
                 curr=curr.next
