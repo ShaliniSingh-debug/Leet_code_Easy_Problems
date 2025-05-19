deb54ae5e907bb9b6f12a7eff677f9e49210308e
ShaliniSingh-debug <75964089+ShaliniSingh-debug@users.noreply.github.com>
2025-05-19 13:10:09 -0400

    Time: 0 ms (100.00%) | Memory: 18.8 MB (74.50%) - LeetSync

---
+++ Leet_code_daily/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        i = 0  # pointer for the last unique element
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        
+        return i + 1
+
+            
+
+            
+
+               
+
+        
