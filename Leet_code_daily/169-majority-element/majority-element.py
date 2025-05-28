31f54b9aed76dbbbff5d3c50474bf45a4faacf5e
ShaliniSingh-debug <75964089+ShaliniSingh-debug@users.noreply.github.com>
2025-05-27 20:17:19 -0400

    Time: 3 ms (82.93%) | Memory: 19.3 MB (87.43%) - LeetSync

--- Leet_code_daily/169-majority-element/majority-element.py
+++ Leet_code_daily/169-majority-element/majority-element.py
@@ -1,24 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        n=len(nums)
-        number={}
-        if n==1:
-            return nums[0]
-        for i in range(n):
-            if nums[i] in number:
-                number[nums[i]] =number[nums[i]]+1
+        ans=-1
+        count=0
+        for num in nums:
+            if count==0:
+                ans=num
+            if ans==num:
+                count+=1
             else:
-                number[nums[i]]=1
-
-                
-        maxNum=nums[0]
-        for i , val in number.items():
-            if val >n//2:
-                maxNum=i
-        return maxNum
-
-
-
-            
-        
+                count-=1
+        return ans
