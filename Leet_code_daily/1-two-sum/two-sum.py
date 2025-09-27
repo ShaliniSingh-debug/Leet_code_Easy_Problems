1707ed3b265c4fa9a42545c04293b1b1c647b9b8
ShaliniSingh-debug <75964089+ShaliniSingh-debug@users.noreply.github.com>
2025-09-27 16:08:36 -0400

    Time: 3 ms (50.11%) | Memory: 18.9 MB (50.92%) - LeetSync

--- Leet_code_daily/1-two-sum/two-sum.py
+++ Leet_code_daily/1-two-sum/two-sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        memo={}
-        for i in range(len(nums)):
-            if nums[i] in memo:
-                return [memo[nums[i]],i]
-            else:
-                memo[target-nums[i]]=i
-
-
+        seen_hasmap={}
+        for i, num in enumerate(nums):
+            seen_num= target-nums[i]
+            if seen_num in seen_hasmap:
+                return [seen_hasmap[seen_num],i] # get the index of found value from hasmap and i which is now containing the value 
+            seen_hasmap[num]=i
+        
         
