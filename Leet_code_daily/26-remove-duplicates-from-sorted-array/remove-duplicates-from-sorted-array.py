c4a20612e78b93bb71befd08edb106dd97913fa9
ShaliniSingh-debug <75964089+ShaliniSingh-debug@users.noreply.github.com>
2025-09-27 17:23:57 -0400

    Time: 3 ms (46.60%) | Memory: 19.1 MB (29.27%) - LeetSync

--- Leet_code_daily/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ Leet_code_daily/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,14 +3,13 @@ class Solution:
         if not nums:
             return 0
 
-        i = 0  # pointer for the last unique element
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                i += 1
-                nums[i] = nums[j]
-        
-        return i + 1
-
+        i=0
+        for j in range(1,len(nums)):
+            if nums[i]!=nums[j]:
+                i +=1
+                nums[i]= nums[j]
+        return i+1
+        # this is two pointer approach where i will be decided j will move and once i and i j are not equal i will move and replace nums[i] with nums[j]
             
 
             
