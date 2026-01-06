5fa854527273adbe7aa35050f02705e55e4e9948
ShaliniSingh-debug <75964089+ShaliniSingh-debug@users.noreply.github.com>
2026-01-06 17:03:23 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.57%) - LeetSync

--- Leet_code_daily/58-length-of-last-word/length-of-last-word.py
+++ Leet_code_daily/58-length-of-last-word/length-of-last-word.py
@@ -1,4 +1,21 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(s.strip().split()[-1])
+        # return len(s.strip().split()[-1]) space = O(n) time = O(n)
+
+        # second approach
+
+        # first calculate the length of the string
+        i = len(s)-1
+       
+
+        # now check fron last to first until you get first " " this after a word
+        while i >= 0 and s[i] == ' ':
+            i -=1
+        char_count =0 
+        # now have to count the words char until you find first  " "
+        while i >= 0 and s[i] != ' ':
+            char_count +=1
+            i -=1
+        return char_count 
+       
         
